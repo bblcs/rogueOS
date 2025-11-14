98fc5823e650cbda2fff3bf49e687af94919977e
evlc <dodgedodgedoggy@gmail.com>
2025-11-14 23:23:21 +0700

    pit

---
+++ src/pit.c
@@ -0,0 +1,50 @@
+#include "pit.h"
+#include "8259.h"
+#include "interrupts.h"
+#include "screen.h"
+
+#define PIT_CHANNEL0_DATA 0x40
+#define PIT_COMMAND 0x43
+
+#define PIT_BASE_FREQUENCY 1193182
+
+static u32 pit_ticks;
+static u32 pit_freq;
+
+// maps to IRQ0, 0x20 currently
+static void pit_handler(const struct interrupt_ctx* ctx)
+{
+        pit_ticks++;
+}
+
+/// requires init_interrupts and init_8259
+void pit_init(u32 freq)
+{
+        pit_freq = freq;
+        u16 divisor = PIT_BASE_FREQUENCY / freq;
+
+        // 00 - channel 0
+        // 11 - Lbyte/Hbyte access
+        // 011 - square wave generator mode
+        // 0 - 16 bit binary mode
+        outb(PIT_COMMAND, 0x36); // 0b00110110
+
+        u8 low = divisor & 0xff;
+        u8 high = (divisor >> 8) & 0xff;
+
+        outb(PIT_CHANNEL0_DATA, low);
+        outb(PIT_CHANNEL0_DATA, high);
+
+        override_interrupt_handler(0x20, pit_handler, INTERRUPT_GATE);
+}
+
+void sleep(u32 ms)
+{
+        u32 ticks = (ms * pit_freq) / 1000;
+        u32 end = pit_ticks + ticks;
+        printf("ticks: %d end: %d\n", ticks, end);
+
+        while (pit_ticks < end) {
+                __asm__ volatile("hlt\n");
+        }
+}
---
+++ src/pit.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "types.h"
+
+void sleep(u32 ms);
+void pit_init(u32 freq);
