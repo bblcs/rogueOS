696657f4c87916df5ba545f7dcc2c9f6a8fe031f
evlc <dodgedodgedoggy@gmail.com>
2025-11-19 22:20:38 +0700

    clean main

--- src/main.c
+++ src/main.c
@@ -8,16 +8,7 @@ void kmain(void)
         init_printer();
         interrupts_setup();
 
-        __asm__ volatile("mov eax, 0\n"
-                         "mov ecx, 1\n"
-                         "mov edx, 2\n"
-                         "mov ebx, 3\n"
-                         "mov ebp, 4\n"
-                         "mov esi, 5\n"
-                         "mov edi, 6\n");
-        // __asm__ volatile("int 0x58\n"); // exp 1
-        // __asm__ volatile("div eax\n"); // exp 2
-        __asm__ volatile("sti\n"); // exp 3
+        __asm__ volatile("sti\n");
 
         endless_loop();
 }
